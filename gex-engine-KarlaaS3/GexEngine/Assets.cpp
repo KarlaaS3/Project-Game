1cb6ec7eed9e816321bc54487e163892e5e96be7
Karla Serrano <kserrano01@mynbcc.ca>
2025-01-31 08:01:36 -0400

    commit

--- gex-engine-KarlaaS3/GexEngine/Assets.cpp
+++ gex-engine-KarlaaS3/GexEngine/Assets.cpp
@@ -1,16 +1,22 @@
 #include "Assets.h"
 #include "MusicPlayer.h"
-#include "json.hpp"
 #include <iostream>
 #include <cassert>
 #include <fstream>
 
-Assets::Assets()
-{}
+Assets::Assets() {}
 
 Assets& Assets::getInstance() {
-    static Assets instance;          // Meyers Singleton implementation
+    static Assets instance;
     return instance;
+}
+
+void Assets::loadFromFile(const std::string path)
+{
+	loadFonts(path);
+	loadTextures(path);
+	loadSounds(path);
+	loadAnimations(path);
 }
 
 void Assets::addFont(const std::string& fontName, const std::string& path) {
@@ -19,11 +25,10 @@ void Assets::addFont(const std::string& fontName, const std::string& path) {
         throw std::runtime_error("Load failed - " + path);
 
     auto rc = _fontMap.insert(std::make_pair(fontName, std::move(font)));
-    if (!rc.second) assert(0); // big problems if insert fails
+    if (!rc.second) assert(0);
 
     std::cout << "Loaded font: " << path << std::endl;
 }
-
 
 void Assets::addSound(const std::string& soundName, const std::string& path) {
     std::unique_ptr<sf::SoundBuffer> sb(new sf::SoundBuffer);
@@ -31,27 +36,21 @@ void Assets::addSound(const std::string& soundName, const std::string& path) {
         throw std::runtime_error("Load failed - " + path);
 
     auto rc = _soundEffects.insert(std::make_pair(soundName, std::move(sb)));
-    if (!rc.second) assert(0); // big problems if insert fails
+    if (!rc.second) assert(0);
 
     std::cout << "Loaded sound effect: " << path << std::endl;
 }
 
-
-void Assets::addTexture(const std::string& textureName, const std::string& path, bool smooth)
-{
+void Assets::addTexture(const std::string& textureName, const std::string& path, bool smooth) {
     _textures[textureName] = sf::Texture();
     if (!_textures[textureName].loadFromFile(path)) {
         std::cerr << "Could not load texture file: " << path << std::endl;
         _textures.erase(textureName);
-    }
-    else {
+    } else {
         _textures.at(textureName).setSmooth(smooth);
         std::cout << "Loaded texture: " << path << std::endl;
     }
 }
-
-
-
 
 const sf::Font& Assets::getFont(const std::string& fontName) const {
     auto found = _fontMap.find(fontName);
@@ -59,200 +58,99 @@ const sf::Font& Assets::getFont(const std::string& fontName) const {
     return *found->second;
 }
 
-
 const sf::SoundBuffer& Assets::getSound(const std::string& soundName) const {
     auto found = _soundEffects.find(soundName);
     assert(found != _soundEffects.end());
     return *found->second;
 }
 
-const sf::Texture& Assets::getTexture(const std::string& textureName) const
-{
+const sf::Texture& Assets::getTexture(const std::string& textureName) const {
     return _textures.at(textureName);
 }
 
-void Assets::loadFonts(const std::string& path)
-{
+void Assets::loadFonts(const std::string& path) {
     std::ifstream confFile(path);
-    if (confFile.fail()) {
-        std::cerr << "Open file " << path << " failed\n";
-        confFile.close();
-        exit(1);
+    if (!confFile) {
+        std::cerr << "Failed to open font file: " << path << std::endl;
+        return;
     }
 
-    std::string token{ "" };
-    confFile >> token;
-    while (confFile) {
+    std::string token;
+    while (confFile >> token) {
         if (token == "Font") {
-            std::string name, path;
-            confFile >> name >> path;
-            addFont(name, path);
-        }
-        else {
-            // ignore rest of line and continue
+            std::string name, filePath;
+            confFile >> name >> filePath;
+            addFont(name, filePath);
+        } else {
             std::string buffer;
             std::getline(confFile, buffer);
         }
-        confFile >> token;
     }
-    confFile.close();
 }
 
-void Assets::loadTextures(const std::string& path)
-{
-    // Read Config file
+void Assets::loadTextures(const std::string& path) {
     std::ifstream confFile(path);
-    if (confFile.fail()) {
-        std::cerr << "Open file: " << path << " failed\n";
-        confFile.close();
-        exit(1);
+    if (!confFile) {
+        std::cerr << "Failed to open texture file: " << path << std::endl;
+        return;
     }
 
-    std::string token{ "" };
-    confFile >> token;
-    while (confFile) {
+    std::string token;
+    while (confFile >> token) {
         if (token == "Texture") {
-            std::string name, path;
-            confFile >> name >> path;
-            addTexture(name, path);
-        }
-        else {
-            // ignore rest of line and continue
+            std::string name, filePath;
+            confFile >> name >> filePath;
+            addTexture(name, filePath);
+        } else {
             std::string buffer;
             std::getline(confFile, buffer);
         }
-        confFile >> token;
     }
-    confFile.close();
 }
 
-void Assets::loadSounds(const std::string& path)
-{
+void Assets::loadSounds(const std::string& path) {
     std::ifstream confFile(path);
-    if (confFile.fail()) {
-        std::cerr << "Open file " << path << " failed\n";
-        confFile.close();
-        exit(1);
+    if (!confFile) {
+        std::cerr << "Failed to open sound file: " << path << std::endl;
+        return;
     }
 
-    std::string token{ "" };
-    confFile >> token;
-    while (confFile) {
+    std::string token;
+    while (confFile >> token) {
         if (token == "Sound") {
-            std::string name, path;
-            confFile >> name >> path;
-            addSound(name, path);
-        }
-        else {
-            // ignore rest of line and continue
+            std::string name, filePath;
+            confFile >> name >> filePath;
+            addSound(name, filePath);
+        } else {
             std::string buffer;
             std::getline(confFile, buffer);
         }
-        confFile >> token;
     }
-    confFile.close();
-}
-
-void Assets::loadJson(const std::string& path) {
-    // Read Config file
-    std::ifstream confFile(path);
-    if (confFile.fail())
-    {
-        std::cerr << "Open file: " << path << " failed\n";
-        confFile.close();
-        exit(1);
-    }
-
-    std::string token{ "" };
-    confFile >> token;
-    while (confFile)
-    {
-        if (token == "JSON")
-        {
-            using json = nlohmann::json;
-            std::string  path;
-            confFile >> path;
-
-            // read the FrameSets from the json file
-            std::ifstream f(path);
-            json data = json::parse(f)["frames"];
-            for (auto i : data) {
-
-                // clean up animation name
-                std::string tmp = i["filename"];
-                std::string::size_type n = tmp.find(" (");
-                if (n == std::string::npos)
-                    n = tmp.find(".png");
-
-                // create IntRect for each frame in animation
-                auto ir = sf::IntRect(i["frame"]["x"], i["frame"]["y"],
-                    i["frame"]["w"], i["frame"]["h"]);
-
-                _frameSets[tmp.substr(0, n)].push_back(ir);
-            }
-            f.close();
-        }
-        else
-        {
-            // ignore rest of line and continue
-            std::string buffer;
-            std::getline(confFile, buffer);
-        }
-        confFile >> token;
-    }
-    confFile.close();
-
 }
 
 void Assets::loadAnimations(const std::string& path) {
-    // Read Config file
     std::ifstream confFile(path);
-    if (confFile.fail())
-    {
-        std::cerr << "Open file: " << path << " failed\n";
-        confFile.close();
-        exit(1);
+    if (!confFile) {
+        std::cerr << "Failed to open animation file: " << path << std::endl;
+        return;
     }
 
-    std::string token{ "" };
-    confFile >> token;
-    while (confFile)
-    {
-        if (token == "Animation")
-        {
+    std::string token;
+    while (confFile >> token) {
+        if (token == "Animation") {
             std::string name, texture, repeat;
             float speed;
             confFile >> name >> texture >> speed >> repeat;
 
-            Animation a(name,
-                getTexture(texture),
-                _frameSets[name],
-                sf::seconds(1 / speed),
-                (repeat == "yes"));
-
+            Animation a(name, getTexture(texture), _frameSets[name], sf::seconds(1 / speed), (repeat == "yes"));
             _animationMap[name] = a;
-        }
-        else
-        {
-            // ignore rest of line and continue
+        } else {
             std::string buffer;
             std::getline(confFile, buffer);
         }
-        confFile >> token;
     }
-    confFile.close();
 }
 
 const Animation& Assets::getAnimation(const std::string& name) const {
     return _animationMap.at(name);
-}
-
-
-void Assets::loadFromFile(const std::string path) {
-    loadFonts(path);
-    loadTextures(path);
-    loadSounds(path);
-
-    loadJson(path);
-    loadAnimations(path);  // requires  _framesets must come after loadJson
 }
